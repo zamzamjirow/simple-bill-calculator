8c0497b9453106dc628bc2cf92989bb4a9703ec3
zamzamjirow <simangclabdi@gmail.com>
2026-09-16 17:12:38 +0300

    Delete simple bill calculator .py

--- simple bill calculator .py
+++
@@ -1,4 +0,0 @@
-price = float(input("Enter the price of one item: "))
-quantity = int(input("Hoe many items  do you want? "))
-total = price * quantity
-print (F"{quantity} items at {price:.2f} each = {total:.2f}")
