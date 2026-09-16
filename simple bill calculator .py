4bc10653daf71a296c2e5cce85bde4dd359c39e1
zamzamjirow <simangclabdi@gmail.com>
2026-09-16 17:00:04 +0300

    Add files via upload

---
+++ simple bill calculator .py
@@ -0,0 +1,4 @@
+price = float(input("Enter the price of one item: "))
+quantity = int(input("Hoe many items  do you want? "))
+total = price * quantity
+print (F"{quantity} items at {price:.2f} each = {total:.2f}")
